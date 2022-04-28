72380b1a4dcf7bd02a0460c4c39bd585a9f2c74c
LizaKoval <63054459+LizaKoval@users.noreply.github.com>
2022-04-28 14:19:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import findspark
-from nltk.corpus import stopwords
 findspark.init()
 from pyspark.sql import SparkSession
-
-# from pyspark.sql.functions import *
 
 if __name__ == "__main__":
     spark = SparkSession.builder.master("local[1]") \
@@ -31,32 +28,4 @@ if __name__ == "__main__":
     for x in cleaned[:15]:
         print(x)
     
-    print("\n")
-
-    #TODO: DELETE IT LATER
-    # cleaned = output.filter(lambda word:  ord not in stopwords)
-
-
-#    print(rdd2_words.collect())
-    # pattern_punct = '[:\;№\$\!-\~was#+Let]'
-#
-#
-#     bin = spark.sparkContext.textFile("input_data/text_with_trash.txt") #created RDD with data from text file
-
-
-# data = [("L", 1), ("I", 20), ("Z", 30), ("A", 40), ("B", 30), ("A", 60)]
-# inputRDD = spark.sparkContext.parallelize(data)
-#
-# listRdd = spark.sparkContext.parallelize([5, 2, 3, 4, 1, 3, 2])
-#
-# print("fold: " + str(listRdd.fold(0,add)))
-# print("first: " +str(inputRDD.first()))
-# print("top: "+str(listRdd.top(2)))
-# print("top: " + str(inputRDD.top(2)))
-#
-# # take, takeOrdered, takeSample
-# print("take : " + str(listRdd.take(2)))
-# # Output: take : 1,2
-# print("takeOrdered : " + str(listRdd.takeOrdered(2)))
-# # Output: takeOrdered : 1,2
-# print("take : " + str(listRdd.takeSample(0,2)))
+ 
