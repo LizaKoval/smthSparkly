43290b9dab0ecd55e3264dfed775cd10ccff1e90
LizaKoval <63054459+LizaKoval@users.noreply.github.com>
2022-04-28 14:24:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ if __name__ == "__main__":
         .appName("NewSparkTry") \
         .getOrCreate()
 
+# -------------------------------------------counting top 10 words in the "Let my people go" song-----------------------------------------------------------
     rdd = spark.sparkContext.textFile("input_data/input.txt")
     rdd3 = rdd.flatMap(lambda x: x.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda x, y: x + y)
     rating = rdd3.map(lambda x: (x[1], x[0])).sortByKey(ascending=False).take(10)
 
     for x in rating:
         print(x)
-# -------------------------------------------second task-----------------------------------------------------------
+# -------------------------------------------removing trash words from the text-----------------------------------------------------------------------------
     rdd2 = spark.sparkContext.textFile("input_data/text_with_trash.csv")
-    # part below is just fir demonstrating that search and filterring for trash words works
+    
+    # part below is just for demonstrating that search and filterring for trash words works
     data = rdd.flatMap(lambda x: x.split(" ")).collect() # collected all data in one dataset from the first txt file
     temparr = []
     temparr = data
     for x in temparr[:15]:
         print(x)
-
+        
+    # removing unnecessary words
     print("\n")
     trash_words = ['was', 'Let', 'is', 'something', 'weird', 'the', 'so', 'happening', 'recently', '\'']
     cleaned = rdd2.flatMap(lambda x: x.split(" ")).filter(lambda x: x not in trash_words).collect()
