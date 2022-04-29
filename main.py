5b229b37143273a03937e331c15c20d2f11be3f7
LizaKoval <liza.koval.01@bk.ru>
2022-04-29 14:59:24 +0300

    Works but inefficient

--- main.py
+++ main.py
@@ -13,29 +13,28 @@ if __name__ == "__main__":
     timer.start()
 # -------------------------------------------counting top 10 words in the "Let my people go" song-----------------------------------------------------------
     text_bin = spark.sparkContext.textFile("input_data/input.txt")
-#      # .persist(pyspark.StorageLevel.MEMORY_AND_DISK)\
+    #persist_text_words = text_bin.flatMap(lambda x: x.split(" ")).persist(pyspark.StorageLevel.MEMORY_AND_DISK).collect()\
+
     rdd_for_txt_file = text_bin.flatMap(lambda x: x.split(" "))\
-        .map(lambda x: (x, 1))\
-        .reduceByKey(lambda x, y: x + y)\
-        .map(lambda x: (x[1], x[0]))\
-        .sortByKey(ascending=False)\
-        .take(10)
+        .map(lambda x: (x,1))\
+        .reduceByKey(lambda x, y: x+y)\
+        .sortBy(lambda x: x[1], ascending=False)\
+        .persist(pyspark.StorageLevel.MEMORY_AND_DISK)
+
+    top_ten = rdd_for_txt_file.take(10)
+
 # поменять reduce на что-то получше
-    for x in rdd_for_txt_file:
+    for x in top_ten:
         print(x)
 
     print('\n')
-# найти способ как отобразить эти 10 слов эффективнее
-#     for x in rdd_for_txt_file:
-#         print(x)
-# я собираю все данные, только чтобы напечатать 15 первых слов, а надо найти  способ вывести первые 15 слов из rdd
 # -------------------------------------------removing trash words from the text-----------------------------------------------------------------------------
     trashwords_bin = spark.sparkContext.textFile("input_data/trash_words.txt")
     unique_trashwords = set(trashwords_bin.flatMap(lambda x: x.split(" ")).collect())
-    #trashwords_bs = spark.sparkContext.broadcast(unique_trashwords) # creating shared broadcast variable
+    trashwords_bs = spark.sparkContext.broadcast(unique_trashwords) # creating shared broadcast variable
 
     resulting_text = text_bin.flatMap(lambda x: x.split(" "))\
-         .filter(lambda word: word not in unique_trashwords)\
+         .filter(lambda word: word not in trashwords_bs.value)\
          .take(10)
 
     for x in resulting_text:
